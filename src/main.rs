578de0f3ca568ccd000824a2bb063d9cf4ee2abb
metalflow <metal.flow@gmail.com>
2025-11-24 12:20:12 -0800

    Gave Suns and PLanets different components and animators ... It may turn out that Planets are simply Suns that don't have POintlIghts in thenm , but for now they are different.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,3 @@
-//! Here we use shape primitives to generate meshes for 3d objects as well as attaching a runtime-generated patterned texture to each 3d object.
-//!
-//! "Shape primitives" here are just the mathematical definition of certain shapes, they're not meshes on their own! A sphere with radius `1.0` can be defined with [`Sphere::new(1.0)`][Sphere::new] but all this does is store the radius. So we need to turn these descriptions of shapes into meshes.
-//!
-//! While a shape is not a mesh, turning it into one in Bevy is easy. In this example we call [`meshes.add(/* Shape here! */)`][Assets<A>::add] on the shape, which works because the [`Assets<A>::add`] method takes anything that can be turned into the asset type it stores. There's an implementation for [`From`] on shape primitives into [`Mesh`], so that will get called internally by [`Assets<A>::add`].
-//!
-//! [`Extrusion`] lets us turn 2D shape primitives into versions of those shapes that have volume by extruding them. A 1x1 square that gets wrapped in this with an extrusion depth of 2 will give us a rectangular prism of size 1x1x2, but here we're just extruding these 2d shapes by depth 1.
-//!
-//! The material applied to these shapes is a texture that we generate at run time by looping through a "palette" of RGBA values (stored adjacent to each other in the array) and writing values to positions in another array that represents the buffer for an 8x8 texture. This texture is then registered with the assets system just one time, with that [`Handle<StandardMaterial>`] then applied to all the shapes in this example.
-//!
-//! The mesh and material are [`Handle<Mesh>`] and [`Handle<StandardMaterial>`] at the moment, neither of which implement `Component` on their own. Handles are put behind "newtypes" to prevent ambiguity, as some entities might want to have handles to meshes (or images, or materials etc.) for different purposes! All we need to do to make them rendering-relevant components is wrap the mesh handle and the material handle in [`Mesh3d`] and [`MeshMaterial3d`] respectively.
-//!
-//! You can toggle wireframes with the space bar except on wasm. Wasm does not support
-//! `POLYGON_MODE_LINE` on the gpu.
-
 use std::f32::consts::PI;
 use std::path::{Path};
 
@@ -36,7 +21,8 @@ fn main() {
         .add_systems(
             Update,
             (
-                rotate,
+                animate_suns,
+                animate_planets,
                 #[cfg(not(target_arch = "wasm32"))]
                 toggle_wireframe,
             ),
@@ -44,9 +30,13 @@ fn main() {
         .run();
 }
 
-/// A marker component for our shapes so we can query them separately from the ground plane
+/// new marker component for Sun objects
 #[derive(Component)]
-struct Shape;
+struct Sun;
+
+/// new marker component for Planet Objects
+#[derive(Component)]
+struct Planet;
 
 fn setup(
     mut commands: Commands,
@@ -142,7 +132,7 @@ fn setup(
                 )
             //)
         ),
-        Shape,
+        Sun,
     )).id();
     //moved the point light inside the Sun to mimic light emission
     commands.spawn((
@@ -175,7 +165,7 @@ fn setup(
                     1.0 + (i as f32),
                 )
             ),
-            Shape,
+            Planet,
         ));
     }
 
@@ -223,9 +213,15 @@ fn setup(
     ));
 }
 
-fn rotate(mut query: Query<&mut Transform, With<Shape>>, time: Res<Time>) {
+fn animate_suns(mut query: Query<&mut Transform, With<Sun>>, time: Res<Time>) {
     for mut transform in &mut query {
         transform.rotate_y(time.delta_secs() / 2.);
+    }
+}
+
+fn animate_planets(mut query: Query<&mut Transform, With<Planet>>, time: Res<Time>) {
+    for mut transform in &mut query {
+        transform.rotate_y(time.delta_secs() / 4.);
     }
 }
 
